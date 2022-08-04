c02ac53160198aa663b6d990dec38c6df173e2d9
Michael Saiba <michaelsaiba84@gmail.com>
2022-08-04 15:28:27 +0300

    Landing page done

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -13,84 +13,12 @@ defineProps({
   <Head title="Welcome" />
 
   <div
-    class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0"
+    class="relative flex items-top justify-center min-h-screen bg-[url('/img/bg.jpg')] dark:bg-gray-900 sm:items-center sm:pt-0"
   >
     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
       <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
         <!-- This example requires Tailwind CSS v2.0+ -->
-        <div class="relative bg-gray-50 overflow-hidden">
-          <div
-            class="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full"
-            aria-hidden="true"
-          >
-            <div class="relative h-full max-w-7xl mx-auto">
-              <svg
-                class="absolute right-full transform translate-y-1/4 translate-x-1/4 lg:translate-x-1/2"
-                width="404"
-                height="784"
-                fill="none"
-                viewBox="0 0 404 784"
-              >
-                <defs>
-                  <pattern
-                    id="f210dbf6-a58d-4871-961e-36d5016a0f49"
-                    x="0"
-                    y="0"
-                    width="20"
-                    height="20"
-                    patternUnits="userSpaceOnUse"
-                  >
-                    <rect
-                      x="0"
-                      y="0"
-                      width="4"
-                      height="4"
-                      class="text-gray-200"
-                      fill="currentColor"
-                    />
-                  </pattern>
-                </defs>
-                <rect
-                  width="404"
-                  height="784"
-                  fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
-                />
-              </svg>
-              <svg
-                class="absolute left-full transform -translate-y-3/4 -translate-x-1/4 md:-translate-y-1/2 lg:-translate-x-1/2"
-                width="404"
-                height="784"
-                fill="none"
-                viewBox="0 0 404 784"
-              >
-                <defs>
-                  <pattern
-                    id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b"
-                    x="0"
-                    y="0"
-                    width="20"
-                    height="20"
-                    patternUnits="userSpaceOnUse"
-                  >
-                    <rect
-                      x="0"
-                      y="0"
-                      width="4"
-                      height="4"
-                      class="text-gray-200"
-                      fill="currentColor"
-                    />
-                  </pattern>
-                </defs>
-                <rect
-                  width="404"
-                  height="784"
-                  fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)"
-                />
-              </svg>
-            </div>
-          </div>
-
+        <div class="relative bg-transparent overflow-hidden">
           <div class="relative pt-6 pb-16 sm:pb-24">
             <main class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
               <div class="text-center">
@@ -101,7 +29,7 @@ defineProps({
                   <span class="block text-light-green-900 xl:inline">POS</span>
                 </h1>
                 <p
-                  class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"
+                  class="mt-3 max-w-md mx-auto text-base text-black italic sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"
                 >
                   Data to enrich sales businesses
                 </p>
