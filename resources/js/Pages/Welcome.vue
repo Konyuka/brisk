2d8459c984e5f6f1431c29ae2b20f766058be093
Michael Saiba <michaelsaiba84@gmail.com>
2022-08-04 15:13:49 +0300

    Font color check

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -98,7 +98,7 @@ defineProps({
                   class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl"
                 >
                   <span class="block xl:inline">Brisk </span>
-                  <span class="block text-indigo-600 xl:inline">POS</span>
+                  <span class="block text-light-green-900 xl:inline">POS</span>
                 </h1>
                 <p
                   class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"
@@ -114,7 +114,7 @@ defineProps({
                     <div class="rounded-md shadow">
                       <Link
                         :href="route('login')"
-                        class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white hover:text-indigo-600 bg-indigo-600 hover:bg-white md:py-4 md:text-lg md:px-10"
+                        class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white hover:text-light-green-900 bg-light-green-900 hover:bg-white md:py-4 md:text-lg md:px-10"
                       >
                         Login
                       </Link>
@@ -125,7 +125,7 @@ defineProps({
                     >
                       <Link
                         :href="route('register')"
-                        class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 hover:text-white bg-white hover:bg-gray-900 md:py-4 md:text-lg md:px-10"
+                        class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md hover:text-light-green-900 text-white hover:bg-white bg-gray-900 md:py-4 md:text-lg md:px-10"
                       >
                         Register
                       </Link>
