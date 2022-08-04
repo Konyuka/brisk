159cebebcec69c69db01d66acc3679252c8279d9
Michael Saiba <michaelsaiba84@gmail.com>
2022-08-04 15:49:24 +0300

    Custom Login and registration

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -20,10 +20,10 @@ defineProps({
         <!-- This example requires Tailwind CSS v2.0+ -->
         <div class="relative bg-transparent overflow-hidden">
           <div class="relative pt-6 pb-16 sm:pb-24">
-            <main class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
+            <main class="mt-48 mx-auto max-w-7xl px-4 sm:mt-24">
               <div class="text-center">
                 <h1
-                  class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl"
+                  class="text-6xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl"
                 >
                   <span class="block xl:inline">Brisk </span>
                   <span class="block text-light-green-900 xl:inline">POS</span>
@@ -31,7 +31,7 @@ defineProps({
                 <p
                   class="mt-3 max-w-md mx-auto text-base text-black italic sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"
                 >
-                  Data to enrich sales businesses
+                  Data to enhance sales businesses
                 </p>
 
                 <div v-if="canLogin">
