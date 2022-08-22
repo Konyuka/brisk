43f4b9dd526d6d9ec14011fd929a34067b823140
Michael Saiba <michaelsaiba84@gmail.com>
2022-08-22 13:38:46 +0300

    vite role up options

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            external: [
+                /^node:.*/,
+            ]
+        }
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
