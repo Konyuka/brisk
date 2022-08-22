6c7621b941e0fcbcfe9a0d76a55df48e3b03847c
Michael Saiba <michaelsaiba84@gmail.com>
2022-08-22 14:04:28 +0300

    include path

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
     build: {
