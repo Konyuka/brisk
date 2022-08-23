fb2ab20e1a0107e048bbe65ed6820358c533c721
Michael Saiba <michaelsaiba84@gmail.com>
2022-08-24 00:09:55 +0300

    Midnight Push

--- resources/js/Pages/Stock.vue
+++ resources/js/Pages/Stock.vue
@@ -93,7 +93,7 @@ const addProduct = () => {
   <AppLayout title="Dashboard">
     <div>
       <div
-        class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-8"
+        class="px-4 py-6 sm:py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-8"
       >
         <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
           <h2
