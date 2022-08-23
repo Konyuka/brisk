7967efae1542a593dc2fa149c25504237c8c72db
Michael Saiba <michaelsaiba84@gmail.com>
2022-08-23 09:40:34 +0300

    Changed App URl for cors

--- config/app.php
+++ config/app.php
@@ -54,7 +54,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://briskpos.herokuapp.com/'),
+    // 'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL'),
 
