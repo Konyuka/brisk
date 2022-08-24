d0283016f2228c4be04bd239e1eb788c57242987
Michael Saiba <michaelsaiba84@gmail.com>
2022-08-24 23:01:06 +0300

    Mobile Edits for Invoice Page

--- resources/js/Pages/Order.vue
+++ resources/js/Pages/Order.vue
@@ -104,22 +104,30 @@ const tax = computed(() => {
       <h2 id="summary-heading" class="sr-only">Billing Summary</h2>
 
       <div class="bg-gray-100 py-6 px-4 sm:px-6 sm:rounded-lg lg:px-8 lg:py-8 lg:grid lg:grid-cols-12 lg:gap-x-8">
-        <dl class="grid grid-cols-2 gap-6 text-sm sm:grid-cols-2 md:gap-x-8 lg:col-span-7">
+        <dl class="grid grid-cols-1 gap-6 text-sm sm:grid-cols-2 md:gap-x-8 lg:col-span-7">
           
           <div>
-            <dt class="font-medium text-gray-900">Payment information</dt>
+            <dt class="font-medium text-gray-900">Payment Method</dt>
             <dd class="-ml-4 -mt-1 flex flex-wrap">
-              <div class="ml-4 mt-4 flex-shrink-0">
+            <select id="country" name="country" autocomplete="country-name" class="ml-3 mt-4 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-green-900 focus:border-green-900 sm:text-sm">
+                <option>Cash</option>
+                <option>Mpesa</option>
+                <option>Visa</option>
+             </select>
+
+              <!-- <div class="ml-4 mt-4 flex-shrink-0">
                 <svg aria-hidden="true" width="36" height="24" viewBox="0 0 36 24" xmlns="http://www.w3.org/2000/svg" class="h-6 w-auto">
                   <rect width="36" height="24" rx="4" fill="#224DBA" />
                   <path d="M10.925 15.673H8.874l-1.538-6c-.073-.276-.228-.52-.456-.635A6.575 6.575 0 005 8.403v-.231h3.304c.456 0 .798.347.855.75l.798 4.328 2.05-5.078h1.994l-3.076 7.5zm4.216 0h-1.937L14.8 8.172h1.937l-1.595 7.5zm4.101-5.422c.057-.404.399-.635.798-.635a3.54 3.54 0 011.88.346l.342-1.615A4.808 4.808 0 0020.496 8c-1.88 0-3.248 1.039-3.248 2.481 0 1.097.969 1.673 1.653 2.02.74.346 1.025.577.968.923 0 .519-.57.75-1.139.75a4.795 4.795 0 01-1.994-.462l-.342 1.616a5.48 5.48 0 002.108.404c2.108.057 3.418-.981 3.418-2.539 0-1.962-2.678-2.077-2.678-2.942zm9.457 5.422L27.16 8.172h-1.652a.858.858 0 00-.798.577l-2.848 6.924h1.994l.398-1.096h2.45l.228 1.096h1.766zm-2.905-5.482l.57 2.827h-1.596l1.026-2.827z" fill="#fff" />
                 </svg>
                 <p class="sr-only">Visa</p>
               </div>
+
               <div class="ml-4 mt-4">
                 <p class="text-gray-900">Ending with 4242</p>
                 <p class="text-gray-600">Expires 02 / 24</p>
-              </div>
+              </div> -->
+
             </dd>
           </div>
         </dl>
@@ -153,6 +161,9 @@ const tax = computed(() => {
                 <div class="text-right mt-4">
                     <button class="font-extrabold text-green-900 text-2xl underline">Finish Sale <i class="ml-2 fas fa-clipboard-check"></i> </button>
                 </div>
+              </div>
+              <div class="mt-7 flex justify-center">
+                <button class="font-extrabold text-green-900 text-2xl underline">Finish Sale <i class="ml-2 fas fa-clipboard-check"></i> </button>
               </div>
             </div>
           </div>
