c3cbc2438ffed93d7bb5cf6d766021387b366e6b
Michael Saiba <michaelsaiba84@gmail.com>
2023-01-19 14:14:05 +0300

    revert the home button

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -56,12 +56,6 @@ defineProps({
                       >
                         Login
                       </Link>
-                      <a
-                        href="https://briskintern.com/"
-                        class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white hover:text-light-green-900 bg-light-green-900 hover:bg-white md:py-4 md:text-lg md:px-10"
-                      >
-                        Home
-                      </a>
                     </div>
                     <div
                       v-if="canRegister"
